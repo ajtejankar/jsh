58328b284b660a008095e792f687fbff02446cb9
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2016-01-30 23:01:45 +0530

    re-factor(jsh.js): Remove duplicate code, use routines from lib
    * Require lib and use parse and serialize from it
    * Integrate new parser
    * Re-factor serialize and isPrimitive

--- bin/jsh.js
+++ bin/jsh.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 //TODO: support BEGIN and END patterns like awk
+//TODO: support reading input json file name from arguments
 //TODO: support multiline actions (better script parser)
 //TODO: support empty pattern which executes for all paths
 //TODO: support json pretty printing function
@@ -10,34 +11,13 @@
 
 function execAction($, $key, $val, action) {
   let pf = console.log, bn = require('path').basename;
+  // jshint -W061
   eval(action);
 }
 
 (() => {
   let minimatch = require('minimatch');
-
-  function isPrimitive(val) {
-    let type = Object.prototype.toString.call(val);
-    return !(type === '[object Object]' || type === '[object Array]');
-  }
-
-  function serialize(obj, paths, currentPath) {
-    paths = paths || {};
-    currentPath = currentPath || '';
-
-    for (let key of Object.keys(obj)) {
-      let val = obj[key];
-      let newPath = `${currentPath}/${key}`;
-
-      if (isPrimitive(val)) {
-        paths[newPath] = val;
-      } else {
-        paths = serialize(val, paths, newPath);
-      }
-    }
-
-    return paths;
-  }
+  let lib = require('../lib');
 
   function throwError(bool, msg) {
     if (!bool) {
@@ -45,49 +25,26 @@ function execAction($, $key, $val, action) {
     }
   }
 
-  function parseScript(script) {
-    let commands = [];
-
-    script.split(/\n|\r\n/).forEach(command => {
-      let separateAt = command.trim().match(/\s/);
-
-      if (!separateAt) return;
-
-      separateAt = separateAt.index + 1;
-      let pattern = command.slice(0, separateAt);
-      let action = command.slice(separateAt).trim();
-      let parseAction = action.match(/\{([^\}]+)\}/);
-
-      if (!pattern) return;
-
-      throwError(!parseAction, `Failed to parse the command: ${command}`);
-      action = parseAction.pop();
-      throwError(!action, `Failed to parse the command: ${command}`);
-      commands.push({pattern: pattern, action: action});
-    });
-
-    return commands;
-  }
-
   let input = '';
   process.stdin.setEncoding('utf-8');
   process.stdin.on('data', data => input += data);
   process.stdin.on('end', () => {
     try {
-      let pairs = serialize(JSON.parse(input));
-      let commands = parseScript(process.argv[2] || '');
-      let paths = Object.keys(pairs);
+      let pairs = lib.serialize(JSON.parse(input));
+      // TODO: path like input from command line is replaced
+      // with windows style paths on git bash, what happens elsewhere?
+      let commands = lib.parse(process.argv[2] || '');
 
-      paths.forEach(pth => {
-        commands.forEach(command => {
-          if (minimatch(pth, command.pattern)) {
+      for (let pth of Object.keys(pairs)) {
+        for (let command of commands) {
+          if (minimatch(pth, command[0])) {
             let val = pairs[pth];
 
-            execAction(pairs, pth, val, command.action);
+            execAction(pairs, pth, val, command[1]);
           }
-        });
-      });
-    } catch(e) {
+        }
+      }
+    } catch (e) {
       console.log(e.stack || e);
     }
   });
